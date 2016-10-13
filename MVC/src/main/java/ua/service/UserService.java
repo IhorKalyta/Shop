6f08ecde57c3017569536e5707254fc4213832f0
kalyta <kalyta.ihor@gmail.com>
2016-10-13 18:41:23 +0300

    add mail verification

--- MVC/src/main/java/ua/service/UserService.java
+++ MVC/src/main/java/ua/service/UserService.java
@@ -6,28 +6,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import ua.entity.User;
-import ua.entity.VerificationToken;
 import ua.form.ClientFilterForm;
 
 public interface UserService {
-	
-	User getUser(String verificationToken);
 
 	User findByLogin(String login);
 
 	User findById(int id);
-	
+
 	User findByMail(String mail);
 	
-	List<User>findAll();
-	
+	User findByUuid (String uuid);
+
+	List<User> findAll();
+
 	Page<User> findAllPageable(Pageable pageable, ClientFilterForm form);
 
 	void save(User user);
 	
-	void createVerificationTokenForUser(User user, String token);
-	
-	VerificationToken getVerificationToken(String VerificationToken);
-	
-	String validateVerificationToken(String token);
+	void confirmEmail(int userId);
+
 }
